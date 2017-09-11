7e78c3750c11e87f90ed9e648b766b7bedabe65d
Andrey Kuzmin <andrey.kuzmin@soundcloud.com>
2017-09-11 17:05:55 +0200

    Elm meetup 4

--- Slides.elm
+++ Slides.elm
@@ -31,7 +31,7 @@ slides : List (Slide Model Message)
 slides =
     [ [ position ( 370, 470 )
             [ richtext
-                "# [Elm Berlin #3](https://www.meetup.com/Elm-Berlin/events/238210075/)"
+                "# [Elm Berlin #4](https://www.meetup.com/Elm-Berlin/events/238210075/)"
             ]
       , custom Model.tangram
       ]
@@ -59,32 +59,17 @@ slides =
       ]
     , [ padded
             [ title "Schedule"
-            , scale 1.3
+            , scale 1.1
                 [ bulletslide
-                    [ "19:00 — Open door"
+                    [ "19:00 — Open Door"
                     , "19:30 — Welcome Message"
-                    , "19:40 — Daniel Bachler: Using Elm for Science"
-                    , "20:10 — Andrey Kuzmin: Introduction to WebGL in Elm"
-                    , "20:40 — Lightning Talks & Discussion"
+                    , "19:40 — Shuhei Kagawa: Building a Pixel Art Editor with Elm"
+                    , "20:10 — Jack Schaedler: Building Interactive Musical Interfaces with Elm"
+                    , "20:40 — Andrey Kuzmin: Striving for a Better Game Engine Design"
+                    , "21:10 — Dániel Vámosi: Prototyping Interactive Applications on a Functional Stack (lightning talk)"
+                    , "21:20 — Discussion"
                     ]
                 ]
-            ]
-      ]
-    , [ padded
-            [ title "Elm Europe 2017"
-            , split
-                [ bulletslide
-                    [ "Takes place in Paris on June 8-9"
-                    , "Keynotes by Evan Czaplicki and Richard Feldman"
-                    , "Organized by the Elm community, for the Elm community"
-                    ]
-                ]
-                [ align Right
-                    [ image ( 360, 240 ) "assets/elmeurope.jpg" ]
-                ]
-            , spacing 50
-            , align Center
-                [ richtext "## [elmeurope.org](https://elmeurope.org)" ]
             ]
       ]
     ]
